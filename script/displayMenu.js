7450190a5f640933896b6f2b90600dc5e24af6a1
ferdif <ferdifh82@gmail.com>
2021-12-05 15:46:59 +0700

    add new function to display menu

--- script/displayMenu.js
+++ script/displayMenu.js
@@ -1,27 +1,46 @@
 // function to display Menu Items from Pseudo API
 
-const displayMenu = ({ menuImage, menuName, menuPrice }) => {
-  return `<div class="menu-items">
-      <img src=${menuImage} class="card-img" alt="Menu Items">
-      <div class="card-body">
-          <p class="card-name">${menuName}</p>
-          <p class="card-text">${menuPrice}</p>
+// CARA 1
+const menuItems = document.getElementById("cards");
+
+const displayMenu = () => {
+  foodMenus.map((menu) => {
+    menuItems.innerHTML += `
+      <div class="menu-items">
+        <img src="${menu.image}" class="card-img" alt="${menu.name}">
+        <div class="card-body">
+            <p class="card-name">${menu.name}</p>
+            <p class="card-text">Rp ${menu.price}.000</p>
+        </div>
       </div>
-      </div>`;
+    `;
+  });
 };
+displayMenu();
 
-let menuItems;
+// CARA 2
+// const displayMenu = ({ menuImage, menuName, menuPrice }) => {
+//   return `<div class="menu-items">
+//       <img src=${menuImage} class="card-img" alt="Menu Items">
+//       <div class="card-body">
+//           <p class="card-name">${menuName}</p>
+//           <p class="card-text">${menuPrice}</p>
+//       </div>
+//       </div>`;
+// };
 
-const getDataFromAPI = () => {
-  return fetch("./menuAPI.json")
-    .then((response) => response.json())
-    .then((dataJson) => {
-      menuItems = dataJson;
-      menuItems.map((items) => {
-        document.getElementById("cards").innerHTML += displayMenu(items);
-      });
-    })
-    .catch((error) => alert(error.message));
-};
+// let menuItems;
 
-getDataFromAPI();
+// const getDataFromAPI = () => {
+//   return fetch("./API/menuAPI.json")
+//     .then((response) => response.json())
+//     .then((dataJson) => {
+//       menuItems = dataJson;
+//       menuItems.map((items) => {
+//         document.getElementById("cards").innerHTML += displayMenu(items);
+//       });
+//     })
+//     .catch((error) => alert(error.message));
+// };
+
+// getDataFromAPI();
